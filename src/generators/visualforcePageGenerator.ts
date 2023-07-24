fa322ceba60a229efb1a8bb53a70e1ef9bc6fee5
WDCi Dev <dev@wdcigroup.net>
2023-07-24 09:02:21 +0800

    generator

---
+++ src/generators/visualforcePageGenerator.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as path from 'path';
+import { CreateUtil } from '../utils';
+import { VisualforcePageOptions } from '../utils/types';
+import { SfdxGenerator } from './sfdxGenerator';
+
+export default class VisualforcePageGenerator extends SfdxGenerator<VisualforcePageOptions> {
+  constructor(args: string | string[], options: VisualforcePageOptions) {
+    super(args, options);
+    this.sourceRootWithPartialPath('visualforcepage');
+  }
+
+  public validateOptions(): void {
+    CreateUtil.checkInputs(this.options.pagename);
+    CreateUtil.checkInputs(this.options.template);
+  }
+
+  public writing(): void {
+    const { template, label, pagename } = this.options;
+    this.fs.copyTpl(
+      this.templatePath(`${template}.page`),
+      this.destinationPath(path.join(this.outputdir, `${pagename}.page`)),
+      {}
+    );
+    this.fs.copyTpl(
+      this.templatePath('_page.page-meta.xml'),
+      this.destinationPath(
+        path.join(this.outputdir, `${pagename}.page-meta.xml`)
+      ),
+      { vfLabel: label, apiVersion: this.apiversion }
+    );
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { TemplateService } from './service/templateService';
+export { TemplateService };
+export * from './utils/types';
+export * from './utils/createUtil';
